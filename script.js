56583f822150bb1da80d43f54b189270ea1b5a87
Marcosta23 <mpc2073@yahoo.com.br>
2022-01-02 15:25:45 -0300

    Eventos de click

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ let click = (color) =>{
     setTimeout(() =>{
         createColorElement(color).classList.remove('select');
         checkOrder();
-    })
+    },250);
 }
 
 // function create color
@@ -90,3 +90,14 @@ let playGame = () => {
 
 }
 
+/*green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));*/
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
